827d478a0da7686990ac4f6870e53b0209870260
leahchow <ctzhouleya@gmail.com>
2021-02-09 10:00:54 -0500

    tablet-layout

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from './components/Nav';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-import Header from './components/Header.js';
-import Background from './components/Background';
-import MyWork from './components/Mywrok';
+import MainContent from './components/Main';
+
+
+
 
 
 const HomePage =()=>{
   return (
     <>
-    <NavBar />
-    <Header />
-    <Background />
-    <MyWork />
+    <MainContent />
     </>
   )
 }
