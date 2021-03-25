b0709e21fd7aad5545c84dcecbdeb3a4632465ad
leahchow <ctzhouleya@gmail.com>
2021-03-25 12:11:25 -0400

    finish-adding-content-for-unicup

--- src/components/Unicup.js
+++ src/components/Unicup.js
@@ -3,6 +3,8 @@ import teashopData from './localJson/teashop.json';
 import{Grid, Cell}from 'react-mdl';
 import './Unicup.scss'
 import componentDisplay from './assect/imgs/unicup-components.png';
+import {Link} from 'react-router-dom';
+import reportSnippet from './assect/imgs/unicup-lightHouse-report.png'
 
 // this is the unicup teashop project
 
@@ -19,33 +21,23 @@ const UnicupWeb = () => {
        </section>
       </div>
       <Grid className="side-margin" style={{justifyContent:"center"}}>
-        <Cell phone={12} tablet={12} col={8}>
+        <Cell phone={12} tablet={12} col={9}>
         <div className="text-center">
            <h4 className="line text-center">Project Intro</h4>
         </div>
-        <p>This project aims to build up a one-page interactive website for a conceptual bubble teashop brand. The website is built with Html, SCSS, and React. The website is hosted and published on Github for public visits.</p>
+        <p>This showcase is an extended part of the branding project. More UX design portion of this project is <Link to="/branding project">here</Link>.The showcase aims to build up a one-page interactive website for a conceptual bubble teashop brand.The website is hosted and published on Github for public visits. In this project, I am playing the role of a front-end web developer.</p>
         </Cell>
-        <Cell phone={12} tablet={12} col={8}>
-          <h5 className="highlight-title">My Role</h5>
-            <p>Front-end Web Developer</p>
+        <Cell phont={12} tablet={12} col={5} style={{justifySelf:"center"}}>
+          <h5 className="highlight-title">Languages and Library:</h5>
+           <ul>
+             <li><p>HTML</p></li>
+             <li><p>SCSS</p></li>
+             <li><p>REACT</p></li>
+             <li><p>REACT Motion Library</p></li>
+             <li><p>AOS Library</p></li>
+           </ul>
         </Cell>
-        <Cell phone={12} tablet={12} col={8}>
-          <div className="text-center">
-           <h4 className="line">Process and Approach</h4>
-           <div style={{margin:"1.5em 0"}}>
-              <h5>structuring &#10145; Building &#10145; Testing/debugging &#10145; Publish</h5>
-           </div>
-          </div>
-           <p>
-           This show case uses the self-created web prototype on Figma as a blueprint. To beign with coding, I used the website prototype as a reference to create the structure of components. Based on the structure, I started building each component in mobile screen size, adding interactive functionality, creating the responsive layout, and transfering customized style into style sheets. After testing and debugging, I published the website on github for public.
-           </p>
-        </Cell>
-        <Cell phont={12} tablet={12} col={8}>
-          <div>
-            <img src={componentDisplay} alt="component display" className="img-size" />
-          </div>
-        </Cell>
-        <Cell phont={12} tablet={12} col={8}>
+        <Cell phont={12} tablet={12} col={4}>
         <section>
           <h5 className="highlight-title">Features:</h5>
           <ul>
@@ -54,6 +46,34 @@ const UnicupWeb = () => {
             ))}
           </ul>
         </section>
+        </Cell>
+        
+        </Grid>
+        <Grid className="side-margin" style={{justifyContent:"center"}}>
+        <Cell phone={12} tablet={12} col={9}>
+          <div className="text-center">
+           <h4 className="line">Process and Approach</h4>
+           <div style={{margin:"1.5em 0"}}>
+              <h5>structuring &#10145; Building &#10145; Testing/debugging &#10145; Publish</h5>
+           </div>
+          </div>
+           <p>
+           This showcase uses the self-created web prototype on Figma as a blueprint. To begin with coding, I used the website prototype as a reference to create the structure of components. Based on the structure, I started building each component from mobile screen size, adding interactive functionality, creating the responsive layout, and transferring customized style into style sheets. After testing and debugging, I deployed and published the website on GitHub for the public.
+           </p>
+        </Cell>
+        <Cell phont={12} tablet={12} col={9}>
+          <div>
+            <img src={componentDisplay} alt="component display" className="large-img" />
+          </div>
+        </Cell>
+        </Grid>
+        <Grid  className="side-margin" style={{justifyContent:"center"}}>
+        <Cell phont={12} tablet={12} col={9}>
+          <div className="text-center">
+            <h4 className="line">Website Optimization</h4>
+          </div>
+            <img src={reportSnippet} alt="light house report snippet" className="large-img"/>
+          <p>To test and improve the quality of the web page, I use the Google Lighthouse to generate a performance report.  From the Google report, I learn which parts need to be improved to optimize the performance. The next step I could improve for this project is to optimize the SEO and performance based on the diagnoses.</p>
         </Cell>
       </Grid>
     </div>
