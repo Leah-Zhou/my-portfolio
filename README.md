07be9e97411f76435821cb5f51bbc53d0703a621
leahchow <ctzhouleya@gmail.com>
2025-05-22 18:33:24 -0400

    update-bbtea-v2

--- README.md
+++ README.md
@@ -79,5 +79,5 @@ step 3: run git push--type "git push" (push all the changes to remote respos)
 step 4: run npm run deploy (deploy all changes)
 step 5: Go to github, make sure in the right branch, then setting page, then go to "pages"
 step 6: In pages, make sure under branch the selection is "gh-pages"
-step 7: update the custom domain: Under the custom domain, type "leahzhou.com" and save
+step 7: update the custom domain: Under the custom domain, type "leahzhou.work" and save
 step 8: test the deployed page and DONE
--- src/components/Branding.js
+++ src/components/Branding.js
@@ -10,6 +10,7 @@ import mindIcon from './assect/icons/mindmap.svg';
 import moodboardIcon from './assect/icons/moodboard-icon.svg';
 import guidelineIcon from './assect/icons/guideline-icon.svg';
 import prototypeIcon from './assect/icons/prototype-icon.svg';
+import displayImg from './assect/imgs/tea-display.png';
 import competitorReport from './assect/imgs/tea-competitor-report.png';
 import userResearch from './assect/imgs/tea-user-research.png';
 import heroImg from './assect/imgs/hero-teashop.jpg';
@@ -98,7 +99,13 @@ const Branding = () => {
                </div>
              ))
            }
-        </Cell>       
+        </Cell>  
+
+        <Cell phone={12} tablet={12} col={12}>
+          <div>
+            <img src={displayImg}  alt="showcase demo" className='large-img'/>
+         </div>
+        </Cell>     
       </Grid>
 
 {/* PROBLEM STATEMENT */}
@@ -119,13 +126,13 @@ const Branding = () => {
       <Cell phone={12} tablet={12} col={10}>
         <div className="line-width center-margin">
             <span>Discovery</span>
-            <h3>Where is the opportunity?</h3>
+            <h3>Where Is The Opportunity?</h3>
         </div>
           <section style={{marginBottom:"40px"}}>
             {uxResearch.approach.map(item=>(
               <div key={uuidv4()}>
                  <div className="line-width center-margin">
-                    <h4 style={{marginTop:"40px"}}>{item.title}</h4>
+                    <h4 style={{margin:"40px 0 8px 0"}}>{item.title}</h4>
                     <p>{item.content}</p>
                  </div>
                  <div>
@@ -146,14 +153,14 @@ const Branding = () => {
           {/* INSIGHT SECTION */}
 
 
-        <Grid className="side-margin" style={{justifyContent:"center"}}>
+        {/* <Grid className="side-margin" style={{justifyContent:"center"}}>
         <Cell phone={12} tablet={12} col={10}>
            <section className='frame-content'>
              <h4 className='sub-title'>Insights:</h4>
              <ul className='two-grid-content'>
             {uxResearch.achievements.map(each=>(
               <li key={uuidv4()}>
-                <h6 style={{fontWeight:"600"}}>{each.title}</h6>
+                <h6 style={{fontWeight:"600", marginBottom:"1em"}}>{each.title}</h6>
                 <div>{each.content.map(each=>(<ul>
                  <li key={uuidv4()}>
                    <p>{each}</p>
@@ -165,13 +172,16 @@ const Branding = () => {
           </ul>
            </section>
         </Cell>
-      </Grid>
+      </Grid> */}
 
       <Grid className="side-margin" style={{justifyContent:"start"}}>
-      <Cell phone={12} tablet={12} col={7}>
+      <Cell  phone={12} tablet={12} col={3}>
+          <h1>BRANDING</h1>
+          </Cell>
+      <Cell phone={12} tablet={12} col={8}>
           <section className="line-width center-margin">
-            <span>Exploration</span>
-            <h3>🎨Create an innovative brand for audience</h3>
+            <span>Branding Exploration</span>
+            <h3 style={{marginBottom:"8px"}}>🎨How to Create a Popular Brand for Audiences</h3>
             <p>{branding.intro}</p>
           </section>
       </Cell>
@@ -184,7 +194,7 @@ const Branding = () => {
         </Cell>
         <Cell phone={12} tablet={12} col={12}>
            <section className='top-margin-md'>
-            <h4 className='sub-title'>Brand positioning statement</h4>
+            <h4 className='sub-title'>📍Position the Brand</h4>
              <section className='rows-group top-margin-md'>
             {branding.achievements.map(each=>(
               <div key={uuidv4()} className='frame-content'>
@@ -198,21 +208,16 @@ const Branding = () => {
 
         {/* UI ACHIEVEMENTS */}
         <Cell phone={12} tablet={12} col={12}>
-          <section className="line-width" style={{margin:"6em auto 0 auto"}}>
-            <span>Design solutions</span>
-            <h3>🎀Apply key UI elements for branding</h3>
-          </section>
-        </Cell>
-        <Cell phone={12} tablet={12} col={12}>
            <section style={{margin:"40px"}}>
+            <h4 className='sub-title' style={{marginBottom:"24px"}}>🎀Establish the Brand in Visuals</h4>
                {uiDesign.achievements.map(each=>(
-                  <div className="two-cols" key={uuidv4()}>
-                    <section>
-                      <img src={each.img} alt="show case" className="large-img" />
+                  <div key={uuidv4()}>
+                   <section className='line-width center-margin' style={{padding:"2em 0 1em 0"}}>
+                      <h5>{each.title}</h5>
+                      <p>{each.content}</p>
                     </section>
                     <section>
-                      <h5>{each.title}</h5>
-                      <span>{each.content}</span>
+                      <img src={each.img} alt="show case" className="large-img" />
                     </section>
                   </div>
                ))}
@@ -224,32 +229,34 @@ const Branding = () => {
 
           <Cell phone={12} tablet={12} col={12}>
           <section className="text-center">
-            <h4 className="line">UI design process</h4>
+            <h1 className="line">Intergrate Branding in UI Design</h1>
           </section>
-            <div className="discovery-section">
+            <div className="two-cols">
+                            <div>
+              <img src={moodboard}  alt="mood board" className="large-img" />
+              <div style={{textAlign:"center"}}><span>Moodboards</span></div>
+              </div>
               <div className='line-width-sm'>
               <h4>Ideation with moodboards</h4>
-              <p>These moodboards served as visual representations of potential design directions, incorporating elements such as color schemes, typography, imagery, and design styles. By curating these mood boards, I ensured that the UI designs resonated with the brand's identity and communicated its values effectively.</p>
-              </div>
-              <div>
-              <img src={moodboard}  alt="mood board" className="img-size" />
-              <div><p style={{textAlign:"center"}}>Moodboards</p></div>
+              <p>These moodboards served as visual representations of potential design directions, incorporating elements such as color schemes, typography, imagery, and design styles. 
+                By curating these mood boards, I ensured that the UI designs resonated with the brand's identity and communicated its values effectively.
+                </p>
               </div>
             </div>
           </Cell>
           <Cell phone={12} tablet={12} col={12}>
-            <div className="discovery-section">
+            <div className="two-cols">
+                <div>
+              <img src={guideline}  alt="design guideline" className="large-img" />
+              <div style={{textAlign:"center"}}><span>Design Guidelines</span></div>
+              </div>
               <div className='line-width-sm'>
               <h4>Create design guideline</h4>
-              <p>TThis guideline outlined key principles, specifications, and standards to maintain visual harmony across all aspects of the user interface, reinforcing the brand's identity and enhancing user experience.</p>
-              </div>
-              <div>
-              <img src={guideline}  alt="design guideline" className="img-size" />
-              <div><p style={{textAlign:"center"}}>Design Guidelines</p></div>
+              <p>This guideline outlined key principles, specifications, and standards to maintain visual harmony across all aspects of the user interface, reinforcing the brand's identity and enhancing user experience.</p>
               </div>
             </div>
           </Cell>
-          <Cell phone={12} tablet={12} col={11}>
+          {/* <Cell phone={12} tablet={12} col={11}>
             <div className="discovery-section">
               <div className='line-width-sm'>
               <h4>Sketch and Prototype</h4>
@@ -260,16 +267,16 @@ const Branding = () => {
               <div><p style={{textAlign:"center"}}>Ideas Sketching</p></div>
               </div>
             </div>
-          </Cell>
+          </Cell> */}
           <Cell phone={12} tablet={12} col={11}>
-            <div className="discovery-section">
+            <div className="two-cols">
+               <div>
+                <img src={designProcess}  alt="design process" className="large-img" />
+                <div style={{textAlign:"center"}}><span >Design Process</span></div>
+                </div>
               <div className='line-width-sm'>
-              <h4>Parallel and iterative design</h4>
-              <p>To generate the best user-centered website layout, I adopted the parallel design and iterative design methods. I designed three design alternatives and sent the prototypes to target users. With the feedback gained from users, I created a single merged design that takes the best ideas from each of the parallel versions. Finally, I proceed with iterative design to further refine the merged design.</p>
-              </div>
-              <div>
-              <img src={designProcess}  alt="design process" className="img-size" />
-              <div><p style={{textAlign:"center"}}>Design Process</p></div>
+               <h4>Parallel and iterative design</h4>
+                <p>To generate the best user-centered website layout, I adopted the parallel design and iterative design methods. I designed three design alternatives and sent the prototypes to target users. With the feedback gained from users, I created a single merged design that takes the best ideas from each of the parallel versions. Finally, I proceed with iterative design to further refine the merged design.</p>
               </div>
             </div>
           </Cell>
@@ -284,10 +291,7 @@ const Branding = () => {
            <p>{frontEnd.intro}</p>
            </section>
            <section>
-             <a  href="https://leah-zhou.github.io/unicupTeaShop/" target="_blank" className=" next-section">
-             <p>View website</p>
-             <img src={arrowSm}  alt="arrow previous"/>
-             </a>
+              <a  href="https://leah-zhou.github.io/unicupTeaShop/" target="_blank" className="btn-link animate-btn"><span>Visit Website</span></a>
              <a  href="https://github.com/Leah-Zhou/unicupTeaShop" target="_blank" className="next-section">
              <p>View code</p>
              <img src={arrowSm}  alt="arrow previous"/>
